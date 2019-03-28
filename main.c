232995a2525bcfcf51093cc54e11c34f17042626
Aaradhanah <43980950+geekaara@users.noreply.github.com>
2019-03-28 21:49:58 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 struct Node{
     int num;
-    struct Node* next;
-};
+    struct node* next;
+}Node;
 struct Node* head;
+void Insert(int x){
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->num = x;
+    temp->next = head;
+    //if(head != NULL) temp -> next = head;
+    head =temp;
+}
+void Print(){
+    struct Node* temp = head;
+    while(temp!=NULL){
+        printf("%d", temp -> num);
+        temp = temp->next;
+    }
+    printf("\n");
+}
 int main()
 {
-    int n;
     head = NULL;
-    head = (struct Node*)malloc(sizeof(struct Node));
-    head -> num = 2;
-    head -> next = NULL;
-    printf("%d",head->num);
+    int n,i,x;
+    printf("Enter the number of elements\n");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf("Enter the number\n");
+        scanf("%d",&x);
+        Insert(x);
+        Print();
+    }
     return 0;
 }
